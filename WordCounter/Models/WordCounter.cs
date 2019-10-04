2b3ab036d7dc7f944756ab6da9d7714d876d0f33
Christine Frank <christine.braun13@gmail.com>
2019-10-04 11:19:46 -0700

    Add functionality to remove punctuation from words

--- WordCounter/Models/WordCounter.cs
+++ WordCounter/Models/WordCounter.cs
@@ -1,15 +1,18 @@
 using System.Collections.Generic;
+using System.Linq;
 
 namespace WordCounter.Models
 {
     public class Counter
     {
+        public char[] Punctuation { get; }
         public string Sentence { get; }
         public string Search { get; }
         public Counter(string sentence, string word)
         {
             Sentence = sentence.ToLower();
             Search = word.ToLower().Trim();
+            Punctuation = new char[]{'\'','"','!','.','?','(',')',',',';',':','`' };
         }
 
         public static bool CheckForContent(string input)
@@ -56,12 +59,46 @@ namespace WordCounter.Models
             return sentenceList;
         }
 
+        public string RemoveOneSidePunctuation(string word)
+        {
+            for(int i = 0; i<word.Length; i++)
+            {
+                bool charRemoved = false;
+                foreach(char c in Punctuation)
+                {
+                    if(c == word[i])
+                    {
+                        word = word.Substring(word.IndexOf(c)+1);
+                        charRemoved = true;
+                    }
+                }
+                if(!charRemoved){
+                    break;
+                }
+            }
+
+            return word;
+        }
+
+        public string RemoveAllPunctuation(string word)
+        {
+            for(int i = 0; i < 2; i++)
+            {
+                word = RemoveOneSidePunctuation(word);
+                word = word.Select(character => character.ToString()).Aggregate<string>((first,second) => second + first);
+            }
+
+            return word;
+        }
+        
+
         public int CountWordsInSentence(List<string> sentenceList)
         {
             int wordCount = 0;
             foreach(string word in sentenceList)
             {
-                wordCount += CheckWord(word);
+                string modifiedWord = RemoveOneSidePunctuation(word);
+                wordCount += CheckWord(modifiedWord);
             }
             return wordCount;
         }
