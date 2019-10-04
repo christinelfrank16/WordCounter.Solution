56b81b326fcb94297c368e68581626fed36a7519
Christine Frank <christine.braun13@gmail.com>
2019-10-04 08:21:23 -0700

    Add boilerplate to csproj files and base structure for cs files

---
+++ WordCounter.Tests/ModelTests/WordCounterTests.cs
@@ -0,0 +1,34 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WordCounter.Models;
+
+namespace WordCounter.Tests
+{
+
+    [TestClass]
+    public class WordCounterTest
+    {
+
+       
+
+        [TestInitialize]
+        public void Startup()
+        {
+            
+
+        }
+
+        [TestCleanup]
+        public void TestClean()
+        {
+            
+        }
+
+        [TestMethod]
+        public void Method_Behavior_Return()
+        {
+            // Arrange
+            // Act
+            // Assert
+        }
+    }
+}
